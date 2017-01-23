e9b30abb39d784d07522a8876173feba80e699bf
Grubber <grubber9527@gmail.com>
2017-01-23 16:06:28 +0800

    新增 声音作品实体类

---
+++ app/src/main/java/com/github/xtorrent/dailytopic/voice/model/Voice.kt
@@ -0,0 +1,22 @@
+package com.github.xtorrent.dailytopic.voice.model
+
+import com.github.xtorrent.dailytopic.db.model.VoiceModel
+import com.google.auto.value.AutoValue
+
+/**
+ * Created by grubber on 2017/1/23.
+ */
+@AutoValue
+abstract class Voice : VoiceModel {
+    companion object {
+        private val _creator: VoiceModel.Creator<Voice> by lazy {
+            VoiceModel.Creator<Voice>(::AutoValue_Voice)
+        }
+
+        val FACTORY = VoiceModel.Factory<Voice>(_creator)
+
+        fun create(title: String, author: String, coverImage: String, tag: String, link: String, playUrl: String? = null): Voice {
+            return _creator.create(title, author, coverImage, tag, link, playUrl)
+        }
+    }
+}
