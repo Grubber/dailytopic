a5e63f04f25eec5e63d9f0b746cc9a9a8cd9b318
Grubber <grubber9527@gmail.com>
2017-01-23 16:39:59 +0800

    新增 获取声音作品分页数据

--- app/src/main/java/com/github/xtorrent/dailytopic/voice/source/remote/VoiceRemoteDataSource.kt
+++ app/src/main/java/com/github/xtorrent/dailytopic/voice/source/remote/VoiceRemoteDataSource.kt
@@ -1,16 +1,37 @@
 package com.github.xtorrent.dailytopic.voice.source.remote
 
+import com.github.xtorrent.dailytopic.core.buildBaseUrl
+import com.github.xtorrent.dailytopic.utils.newJsoupConnection
 import com.github.xtorrent.dailytopic.voice.model.Voice
 import com.github.xtorrent.dailytopic.voice.source.VoiceDataSource
 import rx.Observable
-import rx.lang.kotlin.emptyObservable
+import rx.lang.kotlin.observable
 
 /**
  * Created by grubber on 2017/1/23.
  */
 class VoiceRemoteDataSource : VoiceDataSource {
     override fun getVoiceList(pageNumber: Int): Observable<List<Voice>> {
-        // TODO
-        return emptyObservable()
+        return observable {
+            if (!it.isUnsubscribed) {
+                try {
+                    val document = newJsoupConnection("${buildBaseUrl("voice")}/voice/past?page=$pageNumber").get()
+                    val voices = arrayListOf<Voice>()
+                    document.getElementsByClass("list_box")
+                            .forEach {
+                                val title = it.getElementsByClass("list_author").first().text()
+                                val author = it.getElementsByClass("author_name").first().text()
+                                val coverImage = it.select("img").first().attr("abs:src")
+                                val tag = it.getElementsByClass("voice_tag").first().text()
+                                val link = it.select("a").first().attr("abs:href")
+                                voices += Voice.create(title, author, coverImage, tag, link)
+                            }
+                    it.onNext(voices)
+                    it.onCompleted()
+                } catch (e: Exception) {
+                    it.onError(e)
+                }
+            }
+        }
     }
 }
